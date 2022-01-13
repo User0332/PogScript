cb74ece7eabf786e53f2276262838ddc603ef384
User0332 <carlzfurtado@gmail.com>
2022-01-13 11:43:08 -0500

    fix pogc.py

--- pogc.py
+++ pogc.py
@@ -1,22 +1,23 @@
-def main():
-	from lexer import Lexer
-	from pog_parser2 import Parser
-	from imports import pogscript_builtins
-	from utils import format_error as format
-	from json import loads
-	from json.decoder import JSONDecodeError
-	from time import sleep
-	from os import chdir
-	from os.path import dirname, split as spl
-	from sys import argv, exit
-	from dis import dis
+from lexer import Lexer
+from pog_parser2 import Parser
+from imports import pogscript_builtins
+from utils import format_error as format
+from json import loads	
+from json.decoder import JSONDecodeError
+from time import sleep
+from os import chdir
+from os.path import dirname, split as spl
+from sys import argv, exit
+from dis import dis
 
 
-	if len(argv) == 1:
+
+def main(argc: int, argv: list[str]):
+	if argc == 1:
 		print("Cooler Command Prompt (R) PogScript Compiler")
 		print("Copyright (C) 2022 CCP Inc.")
 		print("\nusage: pogc [filename]")
-		exit(0)
+		return 0
 	else:
 		file = argv[1]
 
@@ -25,7 +26,7 @@ def main():
 			code = f.read()
 	except (FileNotFoundError, PermissionError):
 		print(format("Either the specified file could not be found, or permission was denied."))
-		exit(1)
+		return 1
 	try:
 		with open("E:\\Users\\carlf\\PogScript\\.pogfig", "r") as f:
 			data = f.read().splitlines()
@@ -34,10 +35,10 @@ def main():
 			import_path = data[2]
 	except IndexError:
 		print(format("Data is missing in .pogfig - All the data is critical for correct execution of the program"))
-		exit(1)
+		return 1
 	except FileNotFoundError or PermissionError:
 		print(format(".pogfig is missing or destroyed"))
-		exit(1)
+		return 1
 	except JSONDecodeError:
 		print(format("The file modifiers data in .pogfig is not valid JSON"))
 
@@ -80,6 +81,6 @@ def main():
 		f.write(f"\n{compiled.co_argcount}\n{compiled.co_cellvars}\n{compiled.co_consts}\n{compiled.co_filename}\n{compiled.co_firstlineno}\n{compiled.co_flags}\n{compiled.co_freevars}\n{compiled.co_kwonlyargcount}\n{compiled.co_varnames}")
 
 if __name__ == "__main__":
-	main()
+	exit(main(len(argv), argv)
 
 
