e07e7b8e171b45bf9ac7917ceab08425b5afe9ef
User0332 <78310305+User0332@users.noreply.github.com>
2022-04-13 13:05:37 -0400

    Update assemble.bat

--- src/assemble.bat
+++ src/assemble.bat
@@ -5,7 +5,7 @@ nasm -f win32 %1.asm
 echo Assembly complete.
 
 echo Running Microsoft Linker...
-link /SUBSYSTEM:WINDOWS /ENTRY:start %1.obj *.lib
+link /SUBSYSTEM:%2 /ENTRY:start %1.obj *.lib
 echo Linking complete.
 
 @echo on
